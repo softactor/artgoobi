c0783015975773ed90348a4a0077999a4f0bb588
softactor <tanveerqureshee1@gmail.com>
2020-03-22 15:57:20 +0600

    responsive issue v1.4

--- application/views/layouts/single.php
+++ application/views/layouts/single.php
@@ -1,4 +1,8 @@
 <?php echo $header; ?>
-
+<div class="col-lg-12 col-xl-12 col-md-12 col-sm-12">    
+    <!--Artwork front page start here-->
+    <?php $this->load->view ('artworks/front_artwork');  ?>
+    <!--Artwork front page end here-->
+</div>
 <?php echo $footer; ?>
                 
