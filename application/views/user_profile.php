e2b9630d794b3d26124821fc51ace760db13c385
softactor <tanveerqureshee1@gmail.com>
2020-03-29 09:13:49 +0600

    fixed profile panel v1

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -7,49 +7,58 @@ if (isset($user_logged_in) && !empty($user_logged_in) && $user_logged_in == $use
 }
 ?>
 <div class="row">
-    <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12">
+    <div class="col-md-12 col-sm-12 col-xs-12 col-xl-12">
         <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12 col-xl-12">
-                <div class="row">
-                        <?php echo $profile_left_panel; ?>
-                    <div class="col-md-9" style="padding: 0">
+                <?php echo $profile_left_panel; ?>
+            <div class="col-md-9" style="padding: 0">
 <?php if ($profiler == true && $users_info->status == 1) { ?>
-                            <div class="row">
-                                <div class="col-md-12" style="margin-bottom: 2px;">
-                                    <div class="alert alert-success" id="success_message" style="display: none;">
-                                        <strong>Success!</strong> <span id="message"></span>
-                                    </div>
-                                    <div class="alert alert-danger" id="error_message" style="display: none;">
-                                        <strong>Error!</strong> <span id="message"></span>
-                                    </div>
-                                    <!--<a href="<?php echo base_url() ?>welcome/artist_image_upload">Upload Image</a>-->
-                                </div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xl-12 col-lg-12 clearfix" style="margin-bottom: 2px;">
+                            <div class="alert alert-success" id="success_message" style="display: none;">
+                                <strong>Success!</strong> <span id="message"></span>
                             </div>
-<?php } ?>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <?php
-                                if (isset($artworks_data) && !empty($artworks_data)) {
-                                    ?>
-                                    <div class="front_art_work_wrapper">    
-                                        <?php
-                                        foreach ($artworks_data as $artwork) {
-                                            ?>
-                                            <div class="artwork_image_holder">
-                                                <div class="inner">
-                                                    <a href="<?php echo base_url('welcome/artwork_details/' . $artwork->artist_id . '/' . $artwork->id); ?>">
-                                                        <img src="<?php echo base_url('uploads/artwork/resize/' . $artwork->image_original); ?>" alt="img">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                    <?php } ?>
-                                    </div>
-<?php } ?>
+                            <div class="alert alert-danger" id="error_message" style="display: none;">
+                                <strong>Error!</strong> <span id="message"></span>
                             </div>
+                            <!--<a href="<?php echo base_url() ?>welcome/artist_image_upload">Upload Image</a>-->
                         </div>
-                    </div>  
+                    </div>
+<?php } ?>
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xl-12 col-lg-12 clearfix">
+                        <?php
+                        if (isset($artworks_data) && !empty($artworks_data)) {
+                            ?>
+                            <div class="front_art_work_wrapper">    
+                                <?php
+                                foreach ($artworks_data as $artwork) {
+                                    ?>
+                                    <div class="artwork_image_holder">
+                                        <div class="inner">
+                                            <a href="<?php echo base_url('welcome/artwork_details/' . $artwork->artist_id . '/' . $artwork->id); ?>">
+                                                <img src="<?php echo base_url('uploads/artwork/resize/' . $artwork->image_original); ?>" alt="img">
+                                            </a>
+                                        </div>
+                                    </div>
+                            <?php } ?>
+                            </div>
+                        <div class="front_art_work_wrapper_small_device">
+                        <div class="row">
+                            <?php
+                                foreach ($artworks_data as $artwork) {
+                            ?>
+                                <div class="col-md-12 col-sm-12 col-xl-12 col-lg-12 clearfix">
+                                    <img class="img img-responsive" src="<?php echo base_url('uploads/artwork/' . $artwork->image_original); ?>" alt="img" />
+                                    <a class="btn btn-block btn-success" href="<?php echo base_url('welcome/artwork_details/' . $artwork->artist_id . '/' . $artwork->id); ?>" target="_blank" rel="noopener">View Details</a>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+<?php } ?>
+                    </div>
                 </div>
-            </div>
+            </div>  
         </div>
     </div>
 </div> 
