4ee69d722d59350799c5ac312a30b26449f5d2f2
softactor <tanveerqureshee1@gmail.com>
2020-04-02 11:12:08 +0600

    positioning serach pannel

--- application/views/layouts/top_menu.php
+++ application/views/layouts/top_menu.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-8 col-xl-8 col-lg-8 col-sm-12">
             <nav class="navbar navbar-default">
                 <div class="container">
                     <div class="navbar-header">
@@ -24,53 +24,59 @@
                             <li><a href="<?php echo base_url('welcome/gallery_list'); ?>">Artgoobi Gallery</a></li>
                             <li><a href="<?php echo base_url('welcome/terms_and_conditions'); ?>">Terms & Conditions</a></li>
                             <li><a href="<?php echo base_url('welcome/contact_us'); ?>">Contact Us</a></li>
-                            <li>
-                                <div class="artgoobi_search_box_container">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control search_input_style" placeholder="Search" id="artworksearching">
-                                        <button type="button" class="search_button" onclick="searchArtistArtworks();"><i class="glyphicon glyphicon-search"></i></button> 
-                                    </div>
-                                    <?php
-                                    $user_logged_in = $this->session->userdata('user_logged_in_status');
-                                    if (isset($user_logged_in) && !empty($user_logged_in)) {
-                                        ?>
-                                        <a href="<?php echo base_url() ?>welcome/user_profile">
-                                            <img src="<?php echo base_url(); ?>images/default_avater.png" class="user-image" alt="User Image" width="25" title="<?php echo $this->session->userdata('user_logged_name'); ?>">
-                                            <?php echo $this->session->userdata('user_logged_name'); ?>
-                                        </a>
-                                    <?php } ?>
-                                    <div class="custom_search" onclick="openAdvanceCustomSearch();" title="advance search">Custom search</div>
-                                </div>
-                            </li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <?php
-                                $user_logged_in = $this->session->userdata('user_logged_in_status');
-                            ?>
-                            <li>                        
-                                <?php
-                                    if (isset($user_logged_in) && !empty($user_logged_in)) {
-                                ?>
-                                <a href="#" data-toggle="modal" data-target="#modal_user_logout"><span class="glyphicon glyphicon-log-out"></span></a>
-                                <?php
-                                }
-                                    if (!isset($user_logged_in) && empty($user_logged_in)) {
-                                ?>
-                                    <a href="#" data-toggle="modal" data-target="#modal_userloggin"><span class="glyphicon glyphicon-log-in"></span></a>
-                                    <!--<a id="signup_link" class="btn btn-default btn-flat btn-xs profile_link_style" data-toggle="modal" data-target="#modal_signup">SIGN UP</a>-->
-                                <?php } ?>
-                            </li>
-                            <?php
-                            if (!isset($user_logged_in) && empty($user_logged_in)) {
-                                ?>
-                                <li>
-                                    <a id="signup_link" href="#" data-toggle="modal" data-target="#modal_signup"><span class="glyphicon glyphicon-user"></span></a>
-                                </li>
-                        <?php } ?>
-                        </ul>
+                        </ul>                        
                     </div><!--/.nav-collapse -->
                 </div>
             </nav>
-        </div>        
+        </div>
+        <div class="col-md-4 col-xl-4 col-lg-4 col-sm-12">
+            <div class="artgoobi_search_box_container">
+                <div class="search_box_container_separation">
+                    <div class="form-group">
+                        <input type="text" class="form-control search_input_style" placeholder="Search" id="artworksearching">
+                        <button type="button" class="search_button" onclick="searchArtistArtworks();"><i class="glyphicon glyphicon-search"></i></button> 
+                    </div>
+                </div>
+                <div class="search_box_container_separation">
+                    <?php
+                    $user_logged_in = $this->session->userdata('user_logged_in_status');
+                    if (isset($user_logged_in) && !empty($user_logged_in)) {
+                        ?>
+                        <a href="<?php echo base_url() ?>welcome/user_profile">
+                            <img src="<?php echo base_url(); ?>images/default_avater.png" class="user-image" alt="User Image" width="25" title="<?php echo $this->session->userdata('user_logged_name'); ?>">
+                            <?php echo $this->session->userdata('user_logged_name'); ?>
+                        </a>
+                    <?php } ?>                
+                    <ul id="top_section_profile_access_desktop" class="top_section_profile_access_desktop_style">
+                        <li>
+                            <div class="custom_search" onclick="openAdvanceCustomSearch();" title="advance search">Custom search</div>
+                        </li>
+                        <?php
+                            $user_logged_in = $this->session->userdata('user_logged_in_status');
+                        ?>
+                        <li>                        
+                            <?php
+                                if (isset($user_logged_in) && !empty($user_logged_in)) {
+                            ?>
+                            <a href="#" data-toggle="modal" data-target="#modal_user_logout"><span class="glyphicon glyphicon-log-out"></span></a>
+                            <?php
+                            }
+                                if (!isset($user_logged_in) && empty($user_logged_in)) {
+                            ?>
+                                <a href="#" data-toggle="modal" data-target="#modal_userloggin"><span class="glyphicon glyphicon-log-in"></span></a>
+                                <!--<a id="signup_link" class="btn btn-default btn-flat btn-xs profile_link_style" data-toggle="modal" data-target="#modal_signup">SIGN UP</a>-->
+                            <?php } ?>
+                        </li>
+                        <?php
+                        if (!isset($user_logged_in) && empty($user_logged_in)) {
+                            ?>
+                            <li>
+                                <a id="signup_link" href="#" data-toggle="modal" data-target="#modal_signup"><span class="glyphicon glyphicon-user"></span></a>
+                            </li>
+                    <?php } ?>                        
+                    </ul>
+                </div>            
+            </div>            
+        </div>     
     </div>
 </div>
