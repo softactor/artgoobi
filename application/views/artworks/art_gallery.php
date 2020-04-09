05efe1b155531999ae28050020decce486810273
softactor <tanveerqureshee1@gmail.com>
2020-04-09 13:48:56 +0600

    added lazy loader

--- application/views/artworks/art_gallery.php
+++ application/views/artworks/art_gallery.php
@@ -8,7 +8,7 @@ if (isset($galleries) && !empty($galleries)) {
             <div class="artwork_image_holder">
                 <div class="inner">
                     <a class="example-image-link" href="<?php echo base_url(); ?>uploads/artwork/<?php echo $artwork->image_original; ?>" data-lightbox="example-set" data-title="<a href='<?php echo base_url('welcome/artwork_details/' . $artwork->artist_id . '/' . $artwork->id); ?>'><?php echo $artwork->title; ?></a>">
-                        <img src="<?php echo base_url('uploads/artwork/resize/' . $artwork->image_original); ?>" alt="img">
+                        <img class="lazy" src="<?php echo base_url('uploads/artwork/resize/' . $artwork->image_original); ?>" alt="<?php echo $artwork->title; ?>" title="<?php echo $artwork->title; ?>">
                     </a>
                 </div>
             </div>
@@ -21,7 +21,7 @@ if (isset($galleries) && !empty($galleries)) {
             ?>
             <a href="<?php echo base_url('welcome/artwork_details/' . $artwork->artist_id . '/' . $artwork->id); ?>" rel="noopener">
                 <div class="col-md-12 col-sm-12 col-xl-12 col-lg-12 clearfix">
-                    <img class="img img-responsive" src="<?php echo base_url('uploads/artwork/' . $artwork->image_original); ?>" alt="img" />
+                    <img class="img img-responsive lazy" src="<?php echo base_url('uploads/artwork/' . $artwork->image_original); ?>" alt="<?php echo $artwork->title; ?>" title="<?php echo $artwork->title; ?>" />
                 </div>
             </a>
             <?php } ?>
